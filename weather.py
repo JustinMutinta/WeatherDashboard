99c731e8a66b70e6a9266f15c8e0fa3e4d02a892
JUSTIN L. MUTINTA <justinmutinta@hotmail.com>
2023-11-23 23:10:43 -0600

    Doesn't work but needed to save the work

--- weather.py
+++ weather.py
@@ -1,8 +1,13 @@
 # weather.py
 import argparse
+import json
+import sys
 from configparser import ConfigParser
+from urllib import parse, request, request
 
-def _get_api_key():
+BASE_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+def get_api_key():
     """Fetch the API key from your configuration file.
 
     Expects a configuration file named "secrets.ini" with structure:
@@ -31,6 +36,32 @@ def read_user_cli_args():
     return parser.parse_args()
 
 
+def build_weather_query(city_input, imperial=False):
+    """
+    Builds the URL for an API request to OpenWeather's weather API.
+    Returns: sr: URL formatted for a call to OpenWeather's city name endpoint
+    """
+    api_key = get_api_key()
+    city_name = " ".join(city_input)
+    url_encoded_city_name = parse.quote_plus(city_name)
+    units = "imperial" if imperial else "metric"
+    url = (
+        f"{BASE_WEATHER_API_URL}?q={url_encoded_city_name}"
+        f"&units={units}&appid{api_key}"
+    )
+    return url
+
+
+def get_weather_data(query_url):
+    response = request.urlopen(query_url)
+    data = response.read()
+    return json.loads(data)
+
+
+
+
 if __name__ == "__main__":
     user_args = read_user_cli_args()
-    print(user_args.city, user_args.imperial)
+    query_url = build_weather_query(user_args.city, user_args.imperial)
+    weather_data = get_weather_data(query_url)
+    print(weather_data)
