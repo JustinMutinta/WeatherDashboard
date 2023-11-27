4699c1565937ff84738f075951e594f3c1f3946a
JUSTIN L. MUTINTA <justinmutinta@hotmail.com>
2023-11-26 22:44:02 -0600

    changed up how the graphs show up

--- app.py
+++ app.py
@@ -4,21 +4,30 @@ from dash import Dash, Input, Output, dcc, html
 
 data = (
     pd.read_csv("weather.csv")
-    .query("Location == 'Austin'")
+    .query("Location == 'Dallas'")
     .assign(Date=lambda data: pd.to_datetime(data["Time"], format="%H:%M:%S"))
     .sort_values(by="Time")
 )
 
-app = Dash(__name__)
+
+external_stylesheets = [
+    {
+        "href": (
+            "https://fonts.googleapis.com/css2?"
+            "family=Lato:wght@400;700&display=swap"
+        ),
+        "rel": "stylesheet",
+    },
+]
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(
     children=[
+
         html.H1(children="Temp Analytics"),
-        html.P(
-            children=(
-                "Analyze the temp in cities over time"
-            ),
-        ),
+        html.P(children=("Analyze the temp in cities over time"),),
+
         dcc.Graph(
             figure={
                 "data": [
@@ -26,6 +35,7 @@ app.layout = html.Div(
                         "x": data["Time"],
                         "y": data["Temperature"],
                         "type": "lines",
+                        "hovertemplate": ("%{y:.2f}C<extra></extra>")
                     },
                 ],
                 "layout": {"title": "Average temp changes"}
@@ -38,6 +48,7 @@ app.layout = html.Div(
                         "x": data["Time"],
                         "y": data["Humidity"],
                         "type": "lines",
+                        "hovertemplate": ("%{y:.0f}%<extra></extra>")
                     },
                 ],
                 "layout": {"title": "Average Humidity changes"}
