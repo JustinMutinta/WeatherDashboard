8b2915eb9cd8ebd39a4dbac3dadaabfad453a208
JUSTIN L. MUTINTA <justinmutinta@hotmail.com>
2023-11-28 20:58:12 -0600

    changed up how the graphs show up

--- app.py
+++ app.py
@@ -24,35 +24,49 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(
     children=[
-
-        html.H1(children="Temp Analytics"),
-        html.P(children=("Analyze the temp in cities over time"),),
-
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Time"],
-                        "y": data["Temperature"],
-                        "type": "lines",
-                        "hovertemplate": ("%{y:.2f}C<extra></extra>")
-                    },
-                ],
-                "layout": {"title": "Average temp changes"}
-            },
+        html.Div(
+            children=[
+                html.H1(children="Temp Analytics", className="header-title",),
+                html.P(children=("Analyze the temp in cities over time"), className="header-title",),
+            ], className="header",
         ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Time"],
-                        "y": data["Humidity"],
-                        "type": "lines",
-                        "hovertemplate": ("%{y:.0f}%<extra></extra>")
-                    },
-                ],
-                "layout": {"title": "Average Humidity changes"}
-            },
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="price-chart",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": [
+                                {
+                                    "x": data["Time"],
+                                    "y": data["Temperature"],
+                                    "type": "lines",
+                                    "hovertemplate": ("%{y:.2f}C<extra></extra>")
+                                },
+                            ],
+                            "layout": {"title": "Average temp changes"}
+                        }
+                    ), className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="volume-chart",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": [
+                                {
+                                    "x": data["Time"],
+                                    "y": data["Humidity"],
+                                    "type": "lines",
+                                    "hovertemplate": ("%{y:.0f}%<extra></extra>")
+                                },
+                            ],
+                            "layout": {"title": "Average Humidity changes"}
+                        },
+                    ), className="card",
+                )
+            ], className="wrapper",
         ),
     ]
 )
